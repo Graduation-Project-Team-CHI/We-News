d52a3147f6d9521d5d6775b136286dbee22957bd
Nour Elden Alaa <nourelden515@gmail.com>
2023-03-20 15:52:17 +0200

    add animation

--- app/src/main/java/com/nourelden515/wenews/MainActivity.kt
+++ app/src/main/java/com/nourelden515/wenews/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.nourelden515.wenews
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -19,23 +21,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val navView: BottomNavigationView = binding.navView
         setContentView(binding.root)
+        setNavigation()
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    private fun setNavigation() {
         setSupportActionBar(binding.toolbar)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment,
-                R.id.settingsFragment,
-                R.id.exploreFragment
-            )
-        )
+        setupAppBar()
         navView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
-        /*navController.addOnDestinationChangedListener { _, destination, _ ->
+        setOnNavigationItemSelectedListener(navView, navController)
+        //setupActionBarVisibility(navController)
+    }
+
+    private fun setupActionBarVisibility(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.loginFragment -> {
+                R.id.homeFragment -> {
                     supportActionBar?.hide()
                     binding.toolbar.visibility = View.GONE
                 }
@@ -44,12 +58,69 @@ class MainActivity : AppCompatActivity() {
                     binding.toolbar.visibility = View.VISIBLE
                 }
             }
-        }*/
-
+        }
     }
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_container)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+
+    private fun setupAppBar() {
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.settingsFragment,
+                R.id.exploreFragment
+            )
+        )
+    }
+
+    private fun setOnNavigationItemSelectedListener(
+        navView: BottomNavigationView,
+        navController: NavController
+    ) {
+        navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    navigateFromHome(navController)
+                }
+                R.id.settingsFragment -> {
+                    navigateFromSettings(navController)
+                }
+                R.id.exploreFragment -> {
+                    navigateFromExplore(navController)
+                }
+            }
+            true
+        }
+    }
+
+    private fun navigateFromExplore(navController: NavController) {
+        when (navController.currentDestination?.id) {
+            R.id.homeFragment -> {
+                navController.navigate(R.id.action_homeFragment_to_exploreFragment)
+            }
+            R.id.settingsFragment -> {
+                navController.navigate(R.id.action_settingsFragment_to_exploreFragment)
+            }
+        }
+    }
+
+    private fun navigateFromSettings(navController: NavController) {
+        when (navController.currentDestination?.id) {
+            R.id.homeFragment -> {
+                navController.navigate(R.id.action_homeFragment_to_settingsFragment)
+            }
+            R.id.exploreFragment -> {
+                navController.navigate(R.id.action_exploreFragment_to_settingsFragment)
+            }
+        }
+    }
+
+    private fun navigateFromHome(navController: NavController) {
+        when (navController.currentDestination?.id) {
+            R.id.settingsFragment -> {
+                navController.navigate(R.id.action_settingsFragment_to_homeFragment)
+            }
+            R.id.exploreFragment -> {
+                navController.navigate(R.id.action_exploreFragment_to_homeFragment)
+            }
+        }
     }
 }
