b4bea3f108809b70388c99b002041d38bcc85d88
mayar <yarahassan1711@gmail.com>
2023-04-05 04:05:23 +0200

    init commit

--- app/src/main/java/com/nourelden515/wenews/settings/SettingsFragment.kt
+++ app/src/main/java/com/nourelden515/wenews/settings/SettingsFragment.kt
@@ -1,18 +1,19 @@
 package com.nourelden515.wenews.settings
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.nourelden515.wenews.R
+import com.nourelden515.wenews.authentication.LoginActivity
+import com.nourelden515.wenews.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = SettingsFragment()
-    }
+    private lateinit var binding: FragmentSettingsBinding
 
     private lateinit var viewModel: SettingsViewModel
 
@@ -20,13 +21,22 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding=FragmentSettingsBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+      binding.logOut.setOnClickListener {
+//          requireActivity().run{
+//              startActivity(Intent(this, LoginActivity::class.java))
+//              finish()
+//          }
+          val intent = Intent (view.context, LoginActivity::class.java)
+          startActivity(intent)
+      }
+
     }
+
 
 }
