cd84c63c7c84cd53c1bc757bbe8086901a7d505e
mayar <100476119+mayar1711@users.noreply.github.com>
2023-06-10 19:13:09 +0300

    change password

--- app/src/main/java/com/nourelden515/wenews/ui/settings/ChangePasswordFragment.kt
+++ app/src/main/java/com/nourelden515/wenews/ui/settings/ChangePasswordFragment.kt
@@ -1,93 +1,82 @@
 package com.nourelden515.wenews.ui.settings
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.UserProfileChangeRequest
 import com.nourelden515.wenews.R
 import com.nourelden515.wenews.databinding.FragmentChangePasswordBinding
 
-@Suppress("UNREACHABLE_CODE", "NAME_SHADOWING")
 class ChangePasswordFragment : Fragment() {
     private lateinit var binding:FragmentChangePasswordBinding
-    private lateinit var mAuth: FirebaseAuth
-    private lateinit var mUser: FirebaseUser
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_change_password, container, false)
-        mAuth = FirebaseAuth.getInstance()
-        mUser = mAuth.currentUser!!
+
+        binding = FragmentChangePasswordBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+        auth= FirebaseAuth.getInstance()
         binding.save.setOnClickListener {
-            val currentPassword = binding.yourPass.editText?.text.toString()
-            val newPassword =  binding.newPass.editText?.text.toString()
-            val confirmPassword =  binding.confirmPassword.editText?.text.toString()
-            if (TextUtils.isEmpty(currentPassword)) {
-                binding.yourPass.error = "Enter current password"
-                binding.yourPass.requestFocus()
-                return@setOnClickListener
-            }
+        changePassword()
+      }
+    }
+private fun changePassword()
+{
+    if(binding.yourPass.editText?.text!!.isNotEmpty()&&
+        binding.newPass.editText?.text!!.isNotEmpty()&&
+        binding.confirmPassword.editText?.text!!.isNotEmpty())
+    {
+        if(binding.newPass.editText?.text.toString()==binding.confirmPassword.editText?.text.toString())
+        {
+          val user:FirebaseUser? = auth.currentUser
+            if(user!=null&& user.email!=null)
+            {
+                val credential = EmailAuthProvider
+                    .getCredential(user.email!!, binding.yourPass.editText?.text.toString())
 
-            if (TextUtils.isEmpty(newPassword)) {
-                binding.newPass.error = "Enter new password"
-                binding.newPass.requestFocus()
-                return@setOnClickListener
-            }
-
-            if (TextUtils.isEmpty(confirmPassword)) {
-                binding.confirmPassword.error = "Confirm new password"
-                binding.confirmPassword.requestFocus()
-                return@setOnClickListener
-            }
-
-            if (newPassword != confirmPassword) {
-                binding.confirmPassword.error = "Passwords do not match"
-                binding.confirmPassword.requestFocus()
-                return@setOnClickListener
-            }
-            val credential = EmailAuthProvider.getCredential(mUser.email!!, currentPassword)
-
-            mUser.reauthenticate(credential).addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    mUser.updatePassword(newPassword).addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            val profileUpdates = UserProfileChangeRequest.Builder()
-                                .setDisplayName("New Name")
-                                .build()
-
-                            mUser.updateProfile(profileUpdates)
+            // Prompt the user to re-provide their sign-in credentials
+                user.reauthenticate(credential)
+                    ?.addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Log.d(TAG, "User re-authenticated.")
+                            Toast.makeText(context, "re-authenticated Successful", Toast.LENGTH_SHORT).show()
+                            user!!.updatePassword(binding.newPass.editText?.text.toString())
                                 .addOnCompleteListener { task ->
                                     if (task.isSuccessful) {
-                                        // User profile updated successfully
-                                        Toast.makeText(requireContext(), "User profile updated successfully", Toast.LENGTH_LONG).show()
-
-                                    } else {
-                                        // User profile update failed
-                                        Toast.makeText(requireContext(), "User profile update failed", Toast.LENGTH_LONG).show()
-
+                                        Log.d(TAG, "User password updated.")
+                                        Toast.makeText(context, "Password updated", Toast.LENGTH_SHORT).show()
+                                        findNavController().navigate(R.id.action_changePasswordFragment_to_loginFragment)
                                     }
                                 }
-                        } else {
-                            Toast.makeText(requireContext(), "Password update failed", Toast.LENGTH_LONG).show()
-
+                        }
+                        else{
+                            Toast.makeText(context, "re-authenticated field", Toast.LENGTH_SHORT).show()
                         }
                     }
-                } else {
-                    Toast.makeText(requireContext(), "Re authentication failed", Toast.LENGTH_LONG).show()
-
-                }
             }
-
+            else{
+                findNavController().navigate(R.id.action_changePasswordFragment_to_loginFragment)
+            }
         }
-        return view
+        else {
+            Toast.makeText(context, "Passwords do not match", Toast.LENGTH_SHORT).show()
+        }
     }
+    else{
+        Toast.makeText(context, "please enter all fields", Toast.LENGTH_SHORT).show()
     }
+  }
+}
